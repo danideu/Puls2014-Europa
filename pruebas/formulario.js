0a7ffde8f5b4e3a7b11221d4cb7027f57756b6bc
danideu <danideu@gmail.com>
2014-05-28 17:40:57 +0200

    FInal Clase 5

--- pruebas/formulario.js
+++ pruebas/formulario.js
@@ -1,9 +1,24 @@
-var $form = $('#formulario'),
-	$titulo = $('#titulo'),	
-	$url = $('#link'),
+var $form 		= $('#formulario'),
+	$titulo 	= $('#titulo'),	
+	$url 		= $('#link'),
 	$primerPost = $('.item').first(),
-	$lista = $('#contenido');
+	$lista 		= $('#contenido'),
+	ss    		= sessionStorage,
+	ls 			= localStorage;
 
+//Condición que recupera el valor del titulo y URL de la sesión por 
+//si se ha cerrado la ventana sin querer.
+if (ls.getItem('autosave')){
+	$titulo.val(ss.getItem('titulo'));
+	$url.val(ss.getItem('url'));
+}
+
+//Cada segundo se ejecutará la función anónima 
+//que guardará en las variables de sesión el texto del formulario
+var id = setInterval(function(){
+	ss.setItem('titulo', $titulo.val());
+	ss.setItem('url', $url.val());
+}, 1000); 
 
 function mostrarOcultarFormulario(){
 	$form.slideToggle();
@@ -17,6 +32,23 @@ function validarUrl(){
 		alert('Valid URL')
 	}
 }
+
+
+/*function grabarInformacion(e){
+	e.preventDefault(); 
+
+	var titulo = $titulo.val(),
+		url    = $url.val(),
+		ls     = localStorage, //Guardado durante un tiempo más prolongado.
+		ss	   = sessionStorage; //Lo que dura la sesión del navegador
+
+	ls.secItem('titulo', titulo);
+	ls.secItem('url', url);
+
+	ss.secItem('titulo', titulo);
+	ss.secItem('url', url);
+}*/
+
 //Funcion que agrega un nuevo post en primer lugar dentro del contenido.
 function agregarPost(e){
 	//Para evitar que se envíe el formulario. Es decir, 
@@ -41,22 +73,7 @@ function agregarPost(e){
 	//debugger; //Se para en el código cuando se esté ejecutando.
 }
 
-function grabarInformacion(e){
-	e.preventDefault(); 
-
-	var titulo = $titulo.val(),
-		url    = $url.val(),
-		ls     = localStorage,
-		ss	   = sessionStorage;
-
-	ls.secItem('titulo', titulo);
-	ls.secItem('url', url);
-
-	ss.secItem('titulo', titulo);
-	ss.secItem('url', url);
-}
-
 $('#publicar_nav a').click(mostrarOcultarFormulario);
-//$('#formulario').on('submit', agregarPost);
-$('#formulario').on('submit', grabarInformacion);
+$('#formulario').on('submit', agregarPost);
+//$('#formulario').on('submit', grabarInformacion);
 	
